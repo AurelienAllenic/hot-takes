c42e012a86ef6651b302fcac7361ae9cfc492c01
Aurelien Allenic <aurelien.allenic@gmail.com>
2022-08-29 15:24:06 +0200

    a suppression d'un objet entraîne la suppression de 'image dans le dossier

--- backend/sauces/CRUD/sauce-delete.js
+++ backend/sauces/CRUD/sauce-delete.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const Sauce = require("../models/sauce");
-
+const fs = require("fs");
 module.exports = () => {
-router.delete("/sauces/:id", (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id })
-      .then(() => {
-        res.status(200).json({
-          message: "Deleted!",
+  router.delete("/sauces/:id", (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+      .then((sauce) => {
+        const filename = sauce.imageUrl.split("assets/images/")[1];
+        fs.unlink(`assets/images/${filename}`, () => {
+          Sauce.deleteOne({ _id: req.params.id })
+            .then(() => res.status(200).json({ message: "Sauce supprimée" }))
+            .catch((error) => res.status(400).json({ error }));
+          console.log("Sauce supprimée");
         });
       })
-      .catch((error) => {
-        res.status(400).json({
-          error: error,
-        });
-      });
-    });
-    return router;
-  };
+      .catch((error) => res.status(500).json({ error }));
+  });
+  return router;
+};
